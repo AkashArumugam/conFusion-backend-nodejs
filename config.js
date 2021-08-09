74311a8b2b6c41bb9d2c96a580b34f0ecd135368
AkashArumugam <44131956+AkashArumugam@users.noreply.github.com>
2021-08-09 14:28:34 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/conFusion"
+    "mongoUrl": "mongodb://localhost:27017/conFusion",
+    "facebook": {
+        clientId: "1192823204528164",
+        clientSecret: "e8ec778ac2d50a53dd60ac918eba9ed6"
+    }
 }
